9cf860717eb9b870dc0dd64161bdbe1caf1af3f1
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-19 08:43:03 +0200

    .

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ here.addEventListener('mouseover', function handleMouseOver() {
 here.addEventListener('mouseout', function handleMouseOut() {
     here.style.color = 'black';
 });
+
+
+/*===========================================================*/
+
+
+var showHide = document.getElementById("showHide");
+var demo = document.getElementById("demo");
+showHide.onclick = function() {
+  if (demo.style.display == "block") {
+    demo.style.display = "none";
+  } else {
+    demo.style.display = "block";
+  }
+}
